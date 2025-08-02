0c4184be0da6eb426f2198dbd4628276bd76b830
ashley <leng.ashley12@gmail.com>
2025-08-02 16:28:09 +0800

    Initial Commit

---
+++ sugar rush/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sugar rush
+//
+//  Created by Ashley Leng on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sugar rush/sugar_rushApp.swift
@@ -0,0 +1,17 @@
+//
+//  sugar_rushApp.swift
+//  sugar rush
+//
+//  Created by Ashley Leng on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct sugar_rushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
