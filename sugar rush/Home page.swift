23b5555f1acc97b7b512c91fafce5a518443ca06
Ella38 <ellateozn@gmail.com>
2025-08-30 14:47:54 +0800

    add the score

--- sugar rush/Home page.swift
+++ sugar rush/Home page.swift
@@ -5,45 +5,57 @@
 //  Created by Julia Li on 16/8/25.
 //
 import SwiftUI
+
+class GameData: ObservableObject {
+    @Published var score: Int = 0
+}
+
 struct Home_page: View {
+    @StateObject var gameData = GameData()
     @State var showTutorial = false
     var body: some View {
         NavigationStack {
-            HStack{
-                Spacer()
-                Button{
-                    showTutorial = true
-                }label:{
-                    Text("tutorial")
-                        
-                }
-                
-                    .buttonStyle(.borderedProminent)
-                Spacer()
-                
-                NavigationLink {
-                    Minigames_()
-                } label:{
-                    HStack {
-                        Spacer()
-                        Text("Minigames")
-                        Spacer()
+            VStack{
+                HStack{
+                    Spacer()
+                    Button{
+                        showTutorial = true
+                    }label:{
+                        Text("tutorial")
                         
                     }
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
                     
+                    NavigationLink {
+                        Minigames_()
+                    } label:{
+                        HStack {
+                            Spacer()
+                            Text("Minigames")
+                            Spacer()
+                            
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                    Spacer()
                 }
-                .buttonStyle(.borderedProminent)
                 Spacer()
+                
+                Text("__Amount of Sugar__")
+                    .font(.largeTitle)
+                Text("Score: \(gameData.score)")
+                
+                Spacer()
+                Image("uglyguy")
+                    .resizable()
+                    .frame(width: 400, height: 400)
+                    .navigationTitle(Text("Sugar Rush"))
             }
-            Spacer()
-            Image("uglyguy")
-                .resizable()
-                .frame(width: 400, height: 400)
-            .navigationTitle(Text("Sugar Rush"))
-        }
-        
-        .sheet(isPresented: $showTutorial){
-            tutorialview()
+            
+            .sheet(isPresented: $showTutorial){
+                tutorialview()
+            }
         }
     }
 }
